93f7a33d7a004268f47afff93a15c1c671520221
Lumid-Off <100797807+Lumid-Off@users.noreply.github.com>
2026-09-18 19:32:06 +0300

    chore(release): bump version to 1.2.0

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,12 @@ fn main() -> eframe::Result<()> {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([960.0, 620.0])
             .with_min_inner_size([850.0, 560.0])
-            .with_title("AirCard v1.1.0"),
+            .with_title("AirCard v1.2.0"),
         ..Default::default()
     };
 
     eframe::run_native(
-        "AirCard v1.1.0",
+        "AirCard v1.2.0",
         options,
         Box::new(|cc| Ok(Box::new(app::AirCardApp::new(cc)))),
     )
